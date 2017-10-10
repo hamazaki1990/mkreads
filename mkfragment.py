bdd0a75cb5a600371f1cdbfb78089269c3f0b078
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-10-10 19:10:26 +0900

    mkfragment.py modified

--- mkfragment.py
+++ mkfragment.py
@@ -10,15 +10,22 @@ def make_fragment(file, format, fraglength):
             i += 30
 
 
-fragment = make_fragment("chr1.fa", "fasta", 100)
+chroms = [str(i) for i in range(1, 23)]
+chroms.extend(["X", "Y"])
+print(chroms)
 
-with open("fragment_chr1.fa", "w") as outfile:
-    x = 0
-    while True:
-        seq = next(fragment)
-        record = SeqRecord(seq, id=str(x), description="fragment")
-        if len(seq) < 80:
-            break
-        elif float(seq.count("N")) < 20:
-            SeqIO.write(record, outfile, "fasta")
-        x += 1
+for i in chroms:
+    inputf = "chr"+i+".fa"
+    outputf = "fragment_chr"+i+".fa"
+    fragment = make_fragment(inputf, "fasta", 3)
+
+    with open(outputf, "w") as outfile:
+        x = 0
+        while True:
+            seq = next(fragment)
+            record = SeqRecord(seq, id=str(x), description=inputf+"fragment")
+            if len(seq) < 80:
+                break
+            elif float(seq.count("N")) < 20:
+                SeqIO.write(record, outfile, "fasta")
+            x += 1

5b2eaeb1dfe5aeb3ea935951743b0c7bbdbb24e9
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-10-25 16:27:47 +0900

    success to output

--- mkreads.py
+++ mkreads.py
@@ -27,7 +27,22 @@ def make_reads(file, format, readlength):
             yield [seq1, seq2]
 
 
-read = make_reads("test2.fa", "fasta", 5)
+inputf = "test2.fa"
+read = make_reads(inputf, "fasta", 5)
+outputf1 = "test2_1.fastq"
+outputf2 = "test2_2.fastq"
+x = 0
+while x < 10:
+    paired = next(read)
+    print(pairedreads)
+    rec1 = SeqRecord(paired[0], id="read_"+str(x), description=inputf+"_1")
+    rec2 = SeqRecord(paired[1], id=+"read_"+str(x), description=inputf+"_2")
+    rec1.letter_annotations["phred_quality"] = [40] * len(pairedreads[0])
+    rec2.letter_annotations["phred_quality"] = [40] * len(pairedreads[1])
+    print([rec1, rec2])
 
-print(next(read))
-print(next(read))
+    with open(outputf1, "a") as outfile:
+        SeqIO.write(rec1, outfile, "fastq")
+    with open(outputf2, "a") as outfile:
+        SeqIO.write(rec2, outfile, "fastq")
+    x += 1

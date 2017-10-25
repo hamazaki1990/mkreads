42b6a1481a41f5e257147389a0d46c067ffb528f
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-10-25 15:06:19 +0900

    complete mkreads; outputting next

--- mkreads.py
+++ mkreads.py
@@ -9,26 +9,25 @@ def make_reads(file, format, readlength):
         while True:
             start1 = random.randint(0, reclength-1)
             end1 = start1 + readlength
-            space = random.randrange(200, 500, 50)
-            start2 = start1 + space
+            space = random.randrange(3, 5)
+            start2 = end1 + space
             end2 = start2 + readlength
             if end1 > reclength:
                 seq1 = seq_record.seq[start1:]+seq_record.seq[:end1-reclength]
+                seq2 = seq_record.seq[start2-reclength:end2-reclength]
+            elif start2 > reclength:
+                seq1 = seq_record.seq[start1:end1]
+                seq2 = seq_record.seq[start2-reclength:end2-reclength]
+            elif end2 > reclength:
+                seq1 = seq_record.seq[start1:end1]
+                seq2 = seq_record.seq[start2:]+seq_record.seq[:end2-reclength]
+            else:
+                seq1 = seq_record.seq[start1:end1]
                 seq2 = seq_record.seq[start2:end2]
-            elif start2 :
-                seq1 = seq_record.seq[start:start+readlength]
             yield [seq1, seq2]
 
 
-fragment = make_read(test1.fa, "fasta", 5)
+read = make_reads("test2.fa", "fasta", 5)
 
-    with open(outputf, "w") as outfile:
-        x = 0
-        while True:
-            seq = next(fragment)
-            record = SeqRecord(seq, id=str(x), description=inputf+"fragment")
-            if len(seq) < 80:
-                break
-            elif float(seq.count("N")) < 1:
-                SeqIO.write(record, outfile, "fastq")
-            x += 1
+print(next(read))
+print(next(read))

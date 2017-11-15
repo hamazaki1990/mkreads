5bd742785bdf615ea9fae90826b21189b3ea5e69
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-11-15 19:47:07 +0900

    new: unify_seq.py(not complete)

--- mkreads.py
+++ mkreads.py
@@ -27,26 +27,26 @@ def make_reads(file, format, readlength):
             pairedend.append(seq2.reverse_complement())
             yield pairedend
 
-
-inputf = "DXZ1.fa"
-read = make_reads(inputf, "fasta", 100)
-outputf1 = "DXZ1_artificial_shortread_1.fastq"
-outputf2 = "DXZ1_artificial_shortread_2.fastq"
-x = 0
-while x < 1000000:
-    pairedr = next(read)
-    rec1 = SeqRecord(pairedr[0], id="read_"+str(x), description=inputf+"_1")
-    rec2 = SeqRecord(pairedr[1], id="read_"+str(x), description=inputf+"_2")
-    rec1.letter_annotations["phred_quality"] = [40] * len(pairedr[0])
-    rec2.letter_annotations["phred_quality"] = [40] * len(pairedr[1])
-    if x == 0:
-        with open(outputf1, "w") as outfile:
-            SeqIO.write(rec1, outfile, "fastq")
-        with open(outputf2, "w") as outfile:
-            SeqIO.write(rec2, outfile, "fastq")
-    else:
-        with open(outputf1, "a") as outfile:
-            SeqIO.write(rec1, outfile, "fastq")
-        with open(outputf2, "a") as outfile:
-            SeqIO.write(rec2, outfile, "fastq")
-    x += 1
+for i in range(1,10):
+    inputf = "D1Z5_peri_5end_2.fa"
+    read = make_reads(inputf, "fasta", 100)
+    outputf1 = "%s_D1Z5_peri2_artificial_shortread_1.fastq" %i
+    outputf2 = "%s_D1Z5_peri2_artificial_shortread_2.fastq" %i
+    x = 0
+    while x < 1000000:
+        pairedr = next(read)
+        rec1 = SeqRecord(pairedr[0], id="read_"+str(x), description=inputf+"_1")
+        rec2 = SeqRecord(pairedr[1], id="read_"+str(x), description=inputf+"_2")
+        rec1.letter_annotations["phred_quality"] = [40] * len(pairedr[0])
+        rec2.letter_annotations["phred_quality"] = [40] * len(pairedr[1])
+        if x == 0:
+            with open(outputf1, "w") as outfile:
+                SeqIO.write(rec1, outfile, "fastq")
+            with open(outputf2, "w") as outfile:
+                SeqIO.write(rec2, outfile, "fastq")
+        else:
+            with open(outputf1, "a") as outfile:
+                SeqIO.write(rec1, outfile, "fastq")
+            with open(outputf2, "a") as outfile:
+                SeqIO.write(rec2, outfile, "fastq")
+        x += 1
